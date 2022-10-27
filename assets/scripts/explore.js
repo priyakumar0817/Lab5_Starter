4fc5f90d5d972b3a1799a043aac0f692b607ae77
Priya <priya.kumar817@gmail.com>
2022-10-26 18:57:15 -0700

    first explore push

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,53 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  
+  const voiceSynth = window.speechSynthesis;
+  const options = document.querySelector('select');
+
+  const talkButton = document.querySelector('button');
+  const textToSpeak = document.getElementById('text-to-speak');
+  const imgSmile = document.getElementById('explore').querySelector('img');
+  const voiceParent = document.getElementById('voice-select');
+  const voiceChildren = Array.from(voiceParent);
+  var voices; 
+  function voiceLoad () {
+   
+    voices = voiceSynth.getVoices();
+    for (var i = 0; i < voices.length; i++) {
+      const voiceOptions = document.createElement('option');
+      
+      voiceOptions.value = voices[i].name;
+      voiceOptions.text = voices[i].name + " - (" + voices[i].lang +")"
+      options.appendChild(voiceOptions);
+      
+    }
+  }
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = voiceLoad;
+    }
+
+
+    talkButton.addEventListener('click',speakFunc);
+    function speakFunc () {
+      const utterPhrase = new SpeechSynthesisUtterance(textToSpeak.value);
+      
+      for (var i = 0; i < voices.length; i++) {
+        if (voiceParent.value == voices[i].name) {
+          utterPhrase.voice = voices[i];
+   
+        
+      }
+      
+     
+    }
+    voiceSynth.speak(utterPhrase);
+   
+    imgSmile.src = '/assets/images/smiling-open.png';
+    //console.log(utterPhrase);
+    utterPhrase.addEventListener('end', (endVoice) => {
+      imgSmile.src = '/assets/images/smiling.png';
+    })
+}
+
 }
