b1aee90608369e69d9298d523435cb109d1338c3
Priya Kumar <66938260+priyakumar0817@users.noreply.github.com>
2022-10-30 16:19:32 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -17,14 +17,14 @@ function init() {
     aud.volume = v.target.value / 100;
     const volImg = document.getElementById("volume-controls").querySelector('img');
     if (aud.volume == 0) {
-      volImg.src = '/assets/icons/volume-level-0.svg';
+      volImg.src = './assets/icons/volume-level-0.svg';
     }
     else if (aud.volume < .33) {
-      volImg.src = '/assets/icons/volume-level-1.svg';
+      volImg.src = './assets/icons/volume-level-1.svg';
     } else if (aud.volume > .33 && aud.volume < .67) {
-      volImg.src = '/assets/icons/volume-level-2.svg';
+      volImg.src = './assets/icons/volume-level-2.svg';
     } else {
-      volImg.src = '/assets/icons/volume-level-3.svg';
+      volImg.src = './assets/icons/volume-level-3.svg';
     }
   }
   //button to play sound
@@ -33,13 +33,13 @@ function init() {
   function clickAudio() {
     //audio sounds change based on horn
     if (hornParent.value == 'air-horn') {
-      aud.src = '/assets/audio/air-horn.mp3';
+      aud.src = './assets/audio/air-horn.mp3';
     }
     else if (hornParent.value == 'car-horn') {
-      aud.src = '/assets/audio/car-horn.mp3';
+      aud.src = './assets/audio/car-horn.mp3';
     }
     else if (hornParent.value == 'party-horn') {
-      aud.src = '/assets/audio/party-horn.mp3';
+      aud.src = './assets/audio/party-horn.mp3';
       const jSConfetti = new JSConfetti();
       jSConfetti.addConfetti();
     }
@@ -53,11 +53,11 @@ function init() {
     //change image based on the horn
     const hornPic = document.getElementById("expose").querySelector('img');
     if (hornParent.value == 'air-horn') {
-      hornPic.src = '/assets/images/air-horn.svg';
+      hornPic.src = './assets/images/air-horn.svg';
     } else if (hornParent.value == 'car-horn') {
-      hornPic.src = '/assets/images/car-horn.svg';
+      hornPic.src = './assets/images/car-horn.svg';
     } else if (hornParent.value == 'party-horn') {
-      hornPic.src = '/assets/images/party-horn.svg';
+      hornPic.src = './assets/images/party-horn.svg';
     }
   
   }
